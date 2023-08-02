ae67e373e0fa759dc7d4e9c8b12cd40a7edb97a7
ThomaisSanta <santathomais@hotmail.com>
2023-08-02 13:01:25 +0300

    Create project structure

---
+++ VideoClub/Areas/Copy/CopyAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace VideoClub.Areas.Copy
+{
+    public class CopyAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Copy";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Copy_default",
+                "Copy/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ VideoClub/Areas/MovieRent/MovieRentAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace VideoClub.Areas.MovieRent
+{
+    public class MovieRentAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "MovieRent";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "MovieRent_default",
+                "MovieRent/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}

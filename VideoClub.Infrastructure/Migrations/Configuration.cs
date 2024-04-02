b344c18de04549a4b12ffab6fadacf51b6ea767f
thomaissanta <santathomais@hotmail.com>
2024-04-02 16:05:10 +0300

    Add screenshots

---
+++ VideoClub.Infrastructure/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace VideoClub.Infrastructure.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<VideoClub.Infrastructure.Data.VideoClubContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(VideoClub.Infrastructure.Data.VideoClubContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
